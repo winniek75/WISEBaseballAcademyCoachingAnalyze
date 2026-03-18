d87d83504003a1ef788717902e58bc9d1bad67ec
winniek75 <leapxwise@gmail.com>
2026-03-18 21:54:56 +0900

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import CoachAnalyzer from "./CoachAnalyzer";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <CoachAnalyzer />
-  </StrictMode>
-);
